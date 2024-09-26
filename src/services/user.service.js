a1263378ce27c441333ca7a14686c52c88efb971
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-09-25 22:22:22 -0300

    ADD services

---
+++ src/services/user.service.js
@@ -0,0 +1,43 @@
+"use strict";
+import User from '../entity/user.entity.js';
+import { AppDataSource } from '../config/configDb.js';
+import { formatToLocalTime } from '../utils/formatDate.js'
+
+export async function createUserService(dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const newUser = userRepository.create({
+            nombreCompleto: dataUser.nombreCompleto,
+            rut: dataUser.rut,
+            email: dataUser.email
+        });
+
+        const userSaved = await userRepository.save(newUser);
+
+        return userSaved;
+    } catch (error) {
+        console.error('Error al crear un usuario: ', error);
+    }
+}
+
+export async function getUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id: id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        userFound.createdAt = formatToLocalTime(userFound.createdAt);
+        userFound.updatedAt = formatToLocalTime(userFound.updatedAt);
+
+        return userFound;
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+    }
+}
