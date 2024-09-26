a2604b02e254a2a98e16bc2927bd2273839a4826
DiegoSalazarJara <diego.salazar2101@alumnos.ubiobio.cl>
2024-09-25 21:31:48 -0300

    FIX timezone in updatedAt

--- src/entity/user.entity.js
+++ src/entity/user.entity.js
@@ -28,12 +28,12 @@ const UserSchema = new EntitySchema({
             unique: true,
         },
         createdAt: {
-            type: "time with time zone",
+            type: "timestamp with time zone",
             default: () => "CURRENT_TIMESTAMP",
             nullable: false,
         },
         updatedAt: {
-            type: "time with time zone",
+            type: "timestamp with time zone",
             default: () => "CURRENT_TIMESTAMP",
             onUpdate: "CURRENT_TIMESTAMP",
             nullable: false,
